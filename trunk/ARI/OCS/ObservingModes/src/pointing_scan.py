cdbd2dc4197e155084f25ebbcfdbe5a5343a84ee
salas.mz@gmail.com <salas.mz@gmail.com@203bdf7e-7ac3-ce1b-6535-99cc17bfeca6>
2014-08-26 16:43:33 +0000

    2014-08-26 psalas: added observing scripts

--- trunk/ARI/OCS/ObservingModes/src/pointing_scan.py
+++ trunk/ARI/OCS/ObservingModes/src/pointing_scan.py
@@ -12,15 +12,15 @@ def az_scan(antenna, source, offsets, detector, folder):
     """
 
     print "Starting AZ scan of source %s." % source.name
-
-    scan_data = []
+    
+    detector.set_file_name(folder + "/scan_az_%s.txt" % source.name)
+    
     for i, o in enumerate(offsets):
-        integration = []
         [az, el] = antenna.source_azel(source, antenna.site)
         [az, el] = [az + o, el + 0.0]
         [antenna.aznow, antenna.elnow, antenna.azcount, antenna.elcount, antenna.p.azatstow, antenna.p.elatstow] = \
              antenna.cmd_azel(az, el, antenna.azcount, antenna.elcount, antenna.aznow, antenna.elnow)
-        detector.set_file_name(folder + "/scan_az_%i.txt" % i)
+        #
         detector.get_spectrum()
         detector.write_spectrum(az, el)
 
@@ -36,10 +36,8 @@ def az_scan_both(ant1, ant2, source, offsets, detector, folder, inttime=60):
     
     print "Starting AZ scan of source %s." % source.name
 
-    scan_data = []
     oel = 0
     for i, oaz in enumerate(offsets):
-        integration = []
         # Compute source position and apply azimut offset
         [az1, el1] = ant1.source_azel(source, ant1.site)
         [az2, el2] = ant2.source_azel(source, ant2.site)
@@ -68,10 +66,8 @@ def el_scan(antenna, source, offsets, detector):
     """
 
     print "Starting EL scan of source %s." % source.name
-
-    scan_data = []
+    
     for i, o in enumerate(offsets):
-        integration = []
         [az, el] = antenna.source_azel(source, antenna.site)
         [az, el] = [az + 0.0, el + o]
         [antenna.aznow, antenna.elnow, antenna.azcount, antenna.elcount, antenna.p.azatstow, antenna.p.elatstow] = \
@@ -81,7 +77,33 @@ def el_scan(antenna, source, offsets, detector):
         detector.write_spectrum(az, el)
 
     print "Done with EL scan."
-
+    
+def source_scan(ant1, sou, off, ant2=None, det=None):
+    """
+    Scan the source with antenna at the given offsets.
+    off must be a list of tuples containing the offsets in
+    azimuth and elevation, e.g., off = [(1,0),(2,0),(3,0)],
+    would scan the source at off sets (1,0), (2,0) and (3,0).
+    """
+    detector.set_file_name(folder + "/scan_az_%s.txt" % sou.name)
+    
+    for x,y in off:
+        [az1, el1] = ant1.source_azel(sou, ant1.site)
+        [az1, el1] = [az1 + x, el1 + y]
+        # Move antenna
+        [ant1.aznow, ant1.elnow, ant1.azcount, ant1.elcount, ant1.p.azatstow, ant1.p.elatstow] = \
+             ant1.cmd_azel(az1, el1, ant1.azcount, ant1.elcount, ant1.aznow, ant1.elnow)
+        if ant2:
+            [az2, el2] = ant2.source_azel(sou, ant2.site)
+            [az2, el2] = [az2 + x, el2 + y]
+            [ant2.aznow, ant2.elnow, ant2.azcount, ant2.elcount, ant2.p.azatstow, ant2.p.elatstow] = \
+             ant2.cmd_azel(az2, el2, ant2.azcount, ant2.elcount, ant2.aznow, ant2.elnow)
+        # Take data
+        detector.make_head(ant1=ant1, ant2=ant2, source=sou)
+        detector.get_spectrum()
+        detector.write_spectrum()
+        
+        
 #START OF MAIN:
 if __name__ == "__main__":
     from optparse import OptionParser
