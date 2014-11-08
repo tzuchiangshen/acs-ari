2471247330de12ad197dc0a5c879b3864bd38566
RhoOlguin@gmail.com <RhoOlguin@gmail.com@203bdf7e-7ac3-ce1b-6535-99cc17bfeca6>
2014-11-08 01:34:00 +0000

    ejemplo de ICE invocacion asincrona

--- ARI/TCS/rpiSRT/src/SRTClient.py
+++ ARI/TCS/rpiSRT/src/SRTClient.py
@@ -1,16 +1,32 @@
 import sys, traceback, Ice
 import Demo
 
+
+class MyCallback(object): 
+    def __init__(self):
+        pass
+
+    def getNameCB(self):
+        print "listo"
+
+    def failureCB(self, ex):
+        print "Exception is: " + str(ex)
+
+
 status = 0
 ic = None
 try:
     ic = Ice.initialize(sys.argv)
-    base = ic.stringToProxy("SimplePrinter:default -p 10000")
+    base = ic.stringToProxy("SimplePrinter:default -h 192.168.0.9 -p 10000")
     printer = Demo.PrinterPrx.checkedCast(base)
     if not printer:
         raise RuntimeError("Invalid proxy")
 
-    printer.printString("Hello World!")
+    #printer.printString("Hello World!")
+    cb1 = MyCallback()
+    printer.begin_printString("Hola Asincrono", cb1.getNameCB, cb1.failureCB);
+    print "llegue primero"
+
 except:
     traceback.print_exc()
     status = 1
